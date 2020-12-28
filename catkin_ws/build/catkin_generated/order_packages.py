a7ac8494b271c41b146bd31fd155c981d910846d
SebasRF17 <sebastianseyer@hotmail.com>
2020-12-28 10:39:22 -0600

    catkin_make

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sebasrf/SIAFI/VAE_NPL/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sebasrf/SIAFI/MobileRobots-2020-2-for-Covid19/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/sebasrf/SIAFI/MobileRobots-2020-2-for-Covid19/catkin_ws/devel;/opt/ros/melodic' != '' else []
